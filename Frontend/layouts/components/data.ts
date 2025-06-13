ce1d2cdd3a3dcd5467b25727692c295649a1e2cb
Roberto Zamarripa <roberto@masoft.mx>
2025-06-13 09:10:58 -0700

    actualización al menú

--- Frontend/layouts/components/data.ts
+++ Frontend/layouts/components/data.ts
@@ -84,6 +84,69 @@ export const menuItems: MenuItemType[] = [
     url: "/dashboard",
   },
   {
+    key: "catalogos",
+    label: "Catálogos",
+    icon: TbAlertHexagon,
+    children: [
+      {
+        key: "razonesSociales",
+        label: "Razones Sociales",
+        url: "/catalogos/razonesSociales",
+      },
+      {
+        key: "marcas",
+        label: "Marcas",
+        url: "/catalogos/marcas",
+      },
+      {
+        key: "sucursales",
+        label: "Sucursales",
+        url: "/catalogos/sucursales",
+      },
+      {
+        key: "rutas",
+        label: "Rutas",
+        url: "/catalogos/rutas",
+      },
+
+      {
+        key: "departamentos",
+        label: "Departamentos",
+        url: "/catalogos/departamentos",
+      },
+      {
+        key: "proveedores",
+        label: "Proveedores",
+        url: "/catalogos/proveedores",
+      },
+      {
+        key: "paises",
+        label: "Países",
+        url: "/catalogos/paises",
+      },
+      {
+        key: "estados",
+        label: "Estados",
+        url: "/catalogos/estados",
+      },
+      {
+        key: "municipios",
+        label: "Municipios",
+        url: "/catalogos/municipios",
+      },
+      {
+        key: "bancos",
+        label: "Bancos",
+        url: "/catalogos/bancos",
+      },
+      {
+        key: "conceptosdegastos",
+        label: "Conceptos de Gastos",
+        url: "/catalogos/conceptosDeGastos",
+      },
+    ],
+  },
+  {
     key: "gestion",
     label: "Gestión",
     icon: TbAlertHexagon,
@@ -91,6 +154,45 @@ export const menuItems: MenuItemType[] = [
       { key: "rols", label: "Roles", url: "/gestion/roles" },
       { key: "pages", label: "Paginas", url: "/gestion/pages" },
       { key: "users", label: "Usuarios", url: "/gestion/users" },
+    ],
+  },
+  {
+    key: "paquetes",
+    label: "Paquetes",
+    icon: TbAlertHexagon,
+    children: [
+      {
+        key: "solicitarPaquete",
+        label: "Solicitar Paquete",
+        url: "/paquetes/solicitarPaquete",
+      },
+      {
+        key: "misPaquetes",
+        label: "Mis Paquetes",
+        url: "/paquetes/misPaquetes",
+      },
+      {
+        key: "historialPaquetes",
+        label: "Historial de Paquetes",
+        url: "/paquetes/historialPaquetes",
+      },
+    ],
+  },
+  {
+    key: "presupuestos",
+    label: "Presupuestos",
+    icon: TbAlertHexagon,
+    children: [
+      {
+        key: "asignarPresupuesto",
+        label: "Asignar Presupuesto",
+        url: "/presupuestos/asignarPresupuesto",
+      },
+      {
+        key: "historialPresupuestos",
+        label: "Historial de Presupuestos",
+        url: "/presupuestos/historialPresupuestos",
+      },
     ],
   },
 ];
